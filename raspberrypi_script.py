c84161ff7968176a164f82e42554ad4003d5381a
RyanAnggito <81817722+RyanAnggito@users.noreply.github.com>
2022-09-21 16:40:09 +0700

    Add files via upload

---
+++ raspberrypi_script.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+
+import logging
+from re import S
+
+import dbus
+import dbus.exceptions
+import dbus.mainloop.glib
+import dbus.service
+
+from ble import (
+    Advertisement,
+    Characteristic,
+    Service,
+    Application,
+    find_adapter,
+    Descriptor,
+    Agent,
+)
+
+import struct
+import requests
+import array
+from enum import Enum
+
+import sys
+
+MainLoop = None
+try:
+    from gi.repository import GLib
+
+    MainLoop = GLib.MainLoop
+except ImportError:
+    import gobject as GObject
+
+    MainLoop = GObject.MainLoop
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logHandler = logging.StreamHandler()
+filelogHandler = logging.FileHandler("logs.log")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logHandler.setFormatter(formatter)
+filelogHandler.setFormatter(formatter)
+logger.addHandler(filelogHandler)
+logger.addHandler(logHandler)
+
+
+VivaldiBaseUrl = "XXXXXXXXXXXX"
+
+mainloop = None
+
+BLUEZ_SERVICE_NAME = "org.bluez"
+GATT_MANAGER_IFACE = "org.bluez.GattManager1"
+LE_ADVERTISEMENT_IFACE = "org.bluez.LEAdvertisement1"
+LE_ADVERTISING_MANAGER_IFACE = "org.bluez.LEAdvertisingManager1"
+
+
+class InvalidArgsException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.freedesktop.DBus.Error.InvalidArgs"
+
+
+class NotSupportedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.NotSupported"
+
+
+class NotPermittedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.NotPermitted"
+
+
+class InvalidValueLengthException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.InvalidValueLength"
+
+
+class FailedException(dbus.exceptions.DBusException):
+    _dbus_error_name = "org.bluez.Error.Failed"
+
+
+def register_app_cb():
+    logger.info("GATT application registered")
+
+
+def register_app_error_cb(error):
+    logger.critical("Failed to register application: " + str(error))
+    mainloop.quit()
+
+class Kesehatan_Pencernaan(Service):
+    DSS_UUID = "24a7295e-0b9c-40a9-8fee-9b283679ad95"
+
+    def __init__(self,bus,index):
+        Service.__init__(self, bus, index, self.DSS_UUID, True)
+        self.add_characteristic(DataSendingReceivingCharacteristic(bus, 0, self))
+
+class DataSendingReceivingCharacteristic(Characteristic):
+    uuid = "229405b3-a090-4c4b-8f73-57f9de151832"
+    description = b"Untuk mengirimkan hasil pengecekan kesehatan pencernaan"
+
+    def __init__(self, bus, index, service):
+        Characteristic.__init__(
+            self, bus, index, self.uuid, ['read','write'], service,
+        )
+        self.value = "Sehat kok pencernaannya"
+        self.add_descriptor = CharacteristicUserDescriptionDescriptor(bus, 1, self)
+    
+    def ReadValue(self, options):
+        self.value = "Sehat pencernaannya"
+        return self.value
+
+    def WriteValue(self, value, options):
+        print(value)
+        self.value = value
+
+class CharacteristicUserDescriptionDescriptor(Descriptor):
+    """
+    Writable CUD descriptor.
+    """
+
+    CUD_UUID = "2901"
+
+    def __init__(
+        self, bus, index, characteristic,
+    ):
+
+        self.value = array.array("B", characteristic.description)
+        self.value = self.value.tolist()
+        Descriptor.__init__(self, bus, index, self.CUD_UUID, ["read"], characteristic)
+
+    def ReadValue(self, options):
+        return self.value
+
+    def WriteValue(self, value, options):
+        if not self.writable:
+            raise NotPermittedException()
+        self.value = value
+
+
+class KesehatanPencernaanAdvertisement(Advertisement):
+    def __init__(self, bus, index):
+        Advertisement.__init__(self, bus, index, "peripheral")
+        self.add_manufacturer_data(
+            0xFFFF, [0x70, 0x74],
+        )
+        self.add_service_uuid(Kesehatan_Pencernaan.DSS_UUID)
+
+        self.add_local_name("KesehatanPencernaan")
+        self.include_tx_power = True
+
+
+def register_ad_cb():
+    logger.info("Advertisement registered")
+
+
+def register_ad_error_cb(error):
+    logger.critical("Failed to register advertisement: " + str(error))
+    mainloop.quit()
+
+
+AGENT_PATH = "/com/punchthrough/agent"
+
+
+def main():
+    global mainloop
+
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+
+    # get the system bus
+    bus = dbus.SystemBus()
+    # get the ble controller
+    adapter = find_adapter(bus)
+
+    if not adapter:
+        logger.critical("GattManager1 interface not found")
+        return
+
+    adapter_obj = bus.get_object(BLUEZ_SERVICE_NAME, adapter)
+
+    adapter_props = dbus.Interface(adapter_obj, "org.freedesktop.DBus.Properties")
+
+    # powered property on the controller to on
+    adapter_props.Set("org.bluez.Adapter1", "Powered", dbus.Boolean(1))
+
+    # Get manager objs
+    service_manager = dbus.Interface(adapter_obj, GATT_MANAGER_IFACE)
+    ad_manager = dbus.Interface(adapter_obj, LE_ADVERTISING_MANAGER_IFACE)
+
+    advertisement = KesehatanPencernaanAdvertisement(bus, 0)
+    obj = bus.get_object(BLUEZ_SERVICE_NAME, "/org/bluez")
+
+    agent = Agent(bus, AGENT_PATH)
+
+    app = Application(bus)
+    app.add_service(Kesehatan_Pencernaan(bus, 2))
+
+    mainloop = MainLoop()
+
+    agent_manager = dbus.Interface(obj, "org.bluez.AgentManager1")
+    agent_manager.RegisterAgent(AGENT_PATH, "NoInputNoOutput")
+
+    ad_manager.RegisterAdvertisement(
+        advertisement.get_path(),
+        {},
+        reply_handler=register_ad_cb,
+        error_handler=register_ad_error_cb,
+    )
+
+    logger.info("Registering GATT application...")
+
+    service_manager.RegisterApplication(
+        app.get_path(),
+        {},
+        reply_handler=register_app_cb,
+        error_handler=[register_app_error_cb],
+    )
+
+    agent_manager.RequestDefaultAgent(AGENT_PATH)
+
+    mainloop.run()
+    # ad_manager.UnregisterAdvertisement(advertisement)
+    # dbus.service.Object.remove_from_connection(advertisement)
+
+
+if __name__ == "__main__":
+    main()
